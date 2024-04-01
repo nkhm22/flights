580223b09aed4a09d14f0f3cfd88e6f40a475dad
nkhm22 <nikolaykhmirov@gmail.com>
2024-04-01 14:28:32 +0300

    3

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -6,24 +6,6 @@ from config_data import config
 from states.variables import Variables
 
 
-# @bot.message_handler(commands=['low'])
-# def low(message):
-#     bot.set_state(message.from_user.id, Variables.min_pr, message.chat.id)
-#     bot.send_message(message.chat.id, 'Введите требуемое количество самых дешевых вариантов:')
-#
-#
-# @bot.message_handler(content_types=['text'])
-# def get_low(message):
-#     res_min = requests.get(
-#         f"https://api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=VKO&destination=TBS&departure_at=2024-07-11&return_at=2024-07-20&unique=false&sorting=price&direct=false&cy=usd&limit={Variables.min_pr}&page=1&one_way=true&token={config.RAPID_API_KEY}")
-#     json_dict_min = json.loads(res_min.text)
-#     for elem in json_dict_min['data']:
-#         price = elem['price']
-#         departure_at = elem['departure_at']
-#         return_at = elem['return_at']
-#         bot.send_message(message.from_user.id, f'Дата и время вылета в Тбилиси: {departure_at}\n'
-#                                           f'Дата и время возврата в Москву (Внуково): {return_at}\n'
-#                                           f'Стоимость билетов: {price}\n')
 @bot.message_handler(commands=['low'])
 def handle_register(message: Message):
     bot.set_state(message.from_user.id, Variables.min_pr, message.chat.id)
--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,9 +1,8 @@
 from telebot.types import Message
-
 from loader import bot
 
 
 @bot.message_handler(commands=["start"])
 def bot_start(message: Message):
-    bot.reply_to(message,
+    bot.send_message(message,
     f"Привет, {message.from_user.full_name}! Я покажу тебе информацию об авиарейсах из Внуково в Тбилиси и обратно")

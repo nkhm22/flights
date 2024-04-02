e25aa76d48c66990d6a3745c350a8af954d3bf7b
nkhm22 <nikolaykhmirov@gmail.com>
2024-04-02 17:47:48 +0300

    4

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -1,4 +1,3 @@
-import pprint
 
 from loader import bot
 from telebot.types import Message
@@ -17,13 +16,10 @@ def low(message: Message):
 
 @bot.message_handler(state=Variables.min_price)
 def get_low(message: Message):
-    with (message.from_user.id, message.chat.id) as data:
-        data['min_pr'] = Variables.min_price
     result_minimum = requests.get(
-            f"https://api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=VKO&destination=TBS&departure_at=2024-07-11&return_at=2024-07-20&unique=false&sorting=price&direct=false&cy=usd&limit={int(Variables.min_price)}&page=1&one_way=true&token={config.RAPID_API_KEY}")
+            f"https://api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=VKO&destination=TBS&departure_at=2024-07-11&return_at=2024-07-20&unique=false&sorting=price&direct=false&cy=usd&one_way=true&token={config.RAPID_API_KEY}")
     json_dict_minimum = json.loads(result_minimum.text)
-    pprint.pprint(json_dict_minimum)
-    for element in json_dict_minimum['data']:
+    for element in json_dict_minimum['data'][0:Variables.min_price]:
         price = element['price']
         departure_at = element['departure_at']
         return_at = element['return_at']

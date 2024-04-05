9e058246ebe7f8838f7d7ac509eb67f8f2bf1e61
nkhm22 <nikolaykhmirov@gmail.com>
2024-04-05 19:20:15 +0300

    6

--- handlers/custom_handlers/history.py
+++ handlers/custom_handlers/history.py
@@ -12,12 +12,16 @@ def for_history(text, date_time, user):
 
 
 @bot.message_handler(commands=["history"])
-def get_users(message):
+def history(message):
     conn = sqlite3.connect('my_database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM Commands ORDER BY date_time DESC LIMIT 10')
     users = cursor.fetchall()
-    users_str = "\n".join([str(user[0]) for user in users])
-    bot.send_message(message.chat.id, users_str)
-    conn.close()
+    count = 0
+    for user in users:
+        if str(user[3]) == str(message.from_user.full_name):
+            count += 1
+            bot.send_message(message.chat.id, f'Команда: {str(user[1])}, дата и время запроса: {str(user[2])}')
+            if count == 10:
+                break
 
